f9e8451946ea9a8ac0e77d93dc327b0775b49cf0
Jason Quaccia <jlquaccia@gmail.com>
2016-07-19 14:21:18 -0700

    finished styling blog header section

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -8,7 +8,7 @@
         this.projects = Fixtures.getProjects();
 
         $scope.submitMessage = function() {
-            $.post('https://aqueous-lowlands-24334.herokuapp.com', {inquiry: {name: $scope.name, email: $scope.email, phone_number: $scope.phone_number, message: $scope.message}});
+            $.post('http://localhost:4000/', {inquiry: {name: $scope.name, email: $scope.email, phone_number: $scope.phone_number, message: $scope.message}});
 
             console.log('message sent');
 
--- dist/scripts/factories/Post.js
+++ dist/scripts/factories/Post.js
@@ -1,6 +1,6 @@
 (function() {
   function Post($resource) {
-    return $resource('https://aqueous-lowlands-24334.herokuapp.com/posts/:id.json', {}, {
+    return $resource('http://localhost:4000/posts/:id.json', {}, {
       show: { method: 'GET' },
       update: { method: 'PUT', params: { id: '@id' } },
       delete: { method: 'DELETE', params: { id: '@id' } }
