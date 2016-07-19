ca60b17c605e6d719e91952385f1cbbd7498f2f9
Jason Quaccia <jlquaccia@gmail.com>
2016-07-18 20:38:45 -0700

    loaded up all images to aws

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -4,43 +4,43 @@
         var projects = [
             {
                 title: 'Bloc Chat',
-                imageUrl: '/assets/images/projects/bloc_chat.png',
+                imageUrl: 'https://s3-us-west-1.amazonaws.com/jq2016-revamp/Project+Images/bloc_chat.png',
                 description: 'Coming soon: an actual description.'
             },
             
             {
                 title: 'Bloc Jams',
-                imageUrl: '/assets/images/projects/bloc_jams.png',
+                imageUrl: 'https://s3-us-west-1.amazonaws.com/jq2016-revamp/Project+Images/bloc_jams.png',
                 description: 'Coming soon: an actual description.'
             },
             
             {
                 title: 'The Music Project',
-                imageUrl: '/assets/images/projects/artist_recommendations.png',
+                imageUrl: 'https://s3-us-west-1.amazonaws.com/jq2016-revamp/Project+Images/artist_recommendations.png',
                 description: 'Coming soon: an actual description.'
             },
             
             {
                 title: 'Markitoff',
-                imageUrl: '/assets/images/projects/markitoff.png',
+                imageUrl: 'https://s3-us-west-1.amazonaws.com/jq2016-revamp/Project+Images/markitoff.png',
                 description: 'Coming soon: an actual description.'
             },
             
             {
                 title: 'Simply Put Your Way',
-                imageUrl: '/assets/images/projects/simply-put-your-way.png',
+                imageUrl: 'https://s3-us-west-1.amazonaws.com/jq2016-revamp/Project+Images/simply-put-your-way.png',
                 description: 'Coming soon: an actual description.'
             },
             
             {
                 title: 'Festival Quiz',
-                imageUrl: '/assets/images/projects/festival-quiz.png',
+                imageUrl: 'https://s3-us-west-1.amazonaws.com/jq2016-revamp/Project+Images/festival-quiz.png',
                 description: 'Coming soon: an actual description.'
             },
             
             {
                 title: 'Wikiland',
-                imageUrl: '/assets/images/projects/wikiland.png',
+                imageUrl: 'https://s3-us-west-1.amazonaws.com/jq2016-revamp/Project+Images/wikiland.png',
                 description: 'Coming soon: an actual description.'
             }
         ];
